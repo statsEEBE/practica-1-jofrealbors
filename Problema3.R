ce72c18884784b2db73079dce099ed6e6ab4aece
jofre.albors <jofre.albors@auleseebe.upc.edu>
2024-09-12 12:04:50 +0200

    11

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,11 @@
 #Codigo para problema 3
+
+
+rm(list=ls())
+
+x <- c(-8,5,2,-8,9,5,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+sum(x) #si esta ben copiat, aquest valor és 1, ex1
+
+y<- x[-c(21,2,26)] # treiem els valors 21,2 i 26 i l'anumanem y
+sum(exp(x))-sum(exp(y))#ex2
+y[c(19,24)]#mirar posicions ex 3 (veritat)
